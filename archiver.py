ce693e9f76f1dbf53272c6ce08e98db638df7123
mykesss <mykes1777@gmail.com>
2021-08-14 01:54:52 +0800

    existence is pain

--- archiver.py
+++ archiver.py
@@ -22,7 +22,7 @@ print("\nMessages will be stored inside messages.txt")
 print("\n KEEP THIS CONSOLE AND CHROME WINDOW OPEN, MINIMIZED")
 print("\n CLOSING WILL EXIT THE APPLICATION")
 
-PATH = "driver/chromedriver.exe"
+PATH = "driver/chromedriver"
 driver = webdriver.Chrome(PATH)
 driver.minimize_window()
 
@@ -38,36 +38,46 @@ time.sleep(2)
 loginButton = driver.find_element_by_id("loginbutton")
 loginButton.send_keys(Keys.RETURN)
 lastMessage = 'i wanna die this is not a joke this is a cry for help'
-waitTime = 5
-while True:
-    today = datetime.now()
-    date = today.strftime("%m/%d,%H:%M")
-    time.sleep(waitTime)
-    try:
-        name = driver.find_element_by_css_selector("div[role=row]:last-child>div>div[role=gridcell]>h4>span")
-    except:
-        name = driver.find_element_by_css_selector("div[role=row]:last-child>div>div[role=gridcell]>h4>div>div")
-    name = name.text
-    try:
-        with io.open('messages.txt', 'r', encoding='utf8') as f:
-            reader = f.read()
-            reader = list(reader.split('\n'))
-            reader = reader[-2].split(' - ')
-            lastMessage = reader[1].strip('"')
-    except:
-        pass
-    try: #This code is just pathetic someone pls kill me
+
+def saveMessage():
+    waitTime = 5
+    while True:
+        today = datetime.now()
+        date = today.strftime("%m/%d,%H:%M")
+        time.sleep(waitTime)
         try:
-            messages = driver.find_elements_by_css_selector("div[dir=auto]:last-of-type")[-1]
+            name = driver.find_element_by_css_selector("div[role=row]:last-child>div>div[role=gridcell]>h4>span")
         except:
-            messages = driver.find_elements_by_css_selector("div[dir=auto]")[-1]
-        messages = messages.text
-        if messages != lastMessage:
-            with io.open('messages.txt', 'a', encoding='utf8') as f:
-                text = name + ' - "' + messages + '" - ' + date + "\n"
-                writer = f.write(text)
-                print(text)
-                waitTime = 2
-    except:
-        pass
+            try:
+                name = driver.find_element_by_css_selector("div[role=row]:last-child>div>div[role=gridcell]>h4>div>div")
+            except:
+                saveMessage()
+        name = name.text
+        try:
+            with io.open('messages.txt', 'r', encoding='utf8') as f:
+                reader = f.read()
+                reader = list(reader.split('\n'))
+                reader = reader[-2].split(' - ')
+                lastMessage = reader[1].strip('"')
+        except:
+            pass
+        try: #This code is just pathetic someone pls kill me
+            try:
+                messages = driver.find_elements_by_css_selector("div[dir=auto]:last-of-type")[-1]
+            except:
+                try:
+                    messages = driver.find_elements_by_css_selector("div[dir=auto]")[-1]
+                except:
+                    saveMessage()
+            messages = messages.text
+            if messages != lastMessage:
+                with io.open('messages.txt', 'a', encoding='utf8') as f:
+                    text = name + ' - "' + messages + '" - ' + date + "\n"
+                    writer = f.write(text)
+                    print(text)
+                    waitTime = 2
+        except:
+            pass
+
+saveMessage()
 driver.quit()
